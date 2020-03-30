a6b7fba71a2e6ea67e8f34bd4dffe78c0d670b07
Jonathan Flerez <jhonatanflerez@gmail.com>
2020-03-30 14:45:10 -0500

    add - modificacion modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { PagesModule } from '../pages/pages.module';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NotpagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NotpagefoundComponent
+  ]
+})
+
+export class SharedModule{}
