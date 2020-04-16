18ad6f874484a5669e893c662e0a8ab3b74a7df7
Jonathan Flerez <jhonatanflerez@gmail.com>
2020-04-16 08:03:30 -0500

    Observables y operadores RXJS

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,6 +4,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingComponent } from './account-setting/account-setting.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -14,10 +16,12 @@ const pageRoutes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: 'settings', component: AccountSettingComponent },
+      { path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+      { path: 'progress', component: ProgressComponent, data: {titulo: 'Progress'} },
+      { path: 'graficas1', component: Graficas1Component, data: {titulo: 'Graficas'} },
+      { path: 'settings', component: AccountSettingComponent, data: {titulo: 'Configuraciones'} },
+      { path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
+      { path: 'rxjs', component: RxjsComponent, data: {titulo: 'Rxjs'} },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
   }
